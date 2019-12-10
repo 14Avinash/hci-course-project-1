1b92f52b0e2ec99b1dcc39aa36e05107459c7e75
Nikkolas James Irwin <irwin.nikk@gmail.com>
2019-12-10 01:39:14 -0800

    Account Overview
    Added functionality so that all three types of accounts are able to perform actions from within the account overview via the side buttons.

--- pybank/app_code/graphs.py
+++ pybank/app_code/graphs.py
@@ -1,42 +1,53 @@
 import PyQt5.QtWidgets as qt_widgets
 import PyQt5.QtCore as qt_core
 import PyQt5.QtGui as qt_gui
+from PyQt5.QtWidgets import QGridLayout, QRadioButton
+
 import handlers.userhandler as user_handling
 import pyqt5_global.variables as stack
 import matplotlib.pyplot as plt
 import matplotlib.figure as plt_fig
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import app_code.nav_menu as nav_menu
 
 
 #Login window class and functionality
 class Window_Graphs(qt_widgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('PyBank')
+        self.main_layout = qt_widgets.QVBoxLayout()
+        self.nav_h_layout = qt_widgets.QHBoxLayout()
+        self.graph_layout = qt_widgets.QVBoxLayout()
+        self.setWindowTitle('Show Graphs')
         self.setGeometry(50, 50, 500, 500)
         self.setProperty('Main_Window', True)
 
     def user_interface(self):
-        self.layout = qt_widgets.QVBoxLayout()
+        # Create the navigation menu layout
+        self.nav_h_layout.addWidget(nav_menu.Window())
+        self.nav_h_layout.setContentsMargins(1, 0, 0, 0)
+        self.nav_h_layout.setSpacing(0)
 
-        graph_canvas = Graph_Canvas(self, width=5, height=3)
+        graph_canvas = Graph_Canvas(self, width=5, height=4)
+        self.graph_layout.addWidget(graph_canvas)
 
+        close_window_btn = qt_widgets.QPushButton("Close Window", self)
+        close_window_btn.clicked.connect(self.close_window)
+        self.graph_layout.addWidget(close_window_btn)
 
-        # self.layout.addWidget(static_canvas)
+        # Add the navigation menu and tabs to the main layout
+        self.main_layout.addLayout(self.nav_h_layout)
+        self.main_layout.addLayout(self.graph_layout, stretch=5)
 
-        #Username Entry
-        # self.textUser = qt_widgets.QLineEdit(self)
-        # self.labelUser = qt_widgets.QLabel(self)
-        # self.labelUser.setText('Username')
-        # self.layout.addWidget(self.labelUser, 3, 1)
-        # self.layout.addWidget(self.textUser, 4, 1, 1, 3)
-
-        self.setLayout(self.layout)
-
+        self.setLayout(self.main_layout)
         self.show()
 
+    def close_window(self):
+        self.close()
+
+
 class Graph_Canvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=5, dpi=100):
+    def __init__(self, parent=None, width=6, height=10, dpi=100):
         #Graph Canvas
         self.fig = plt_fig.Figure(figsize=(width, height))
         self.axes = self.fig.add_subplot(111)
@@ -46,6 +57,12 @@ class Graph_Canvas(FigureCanvas):
         self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], color="#bb86fc")
         self.axes.plot([0, 1, 2, 3], [2, 4, 3, 3], color="#8858c8")
         self.axes.plot([0, 1, 2, 3], [1, 4, 0, 6], color="#efb7ff")
+        self.axes.set_ylabel("Spending")
+        self.axes.set_xlabel("Time")
+        self.axes.xaxis.label.set_color('white')
+        self.axes.yaxis.label.set_color('white')
+        self.fig.subplots_adjust(left=0.2, right=0.9, bottom=0.3, top=0.9)
+
     def figure_colors(self):
         self.fig.set_facecolor('#180f20')
         self.axes.set_facecolor('#3e3547')
